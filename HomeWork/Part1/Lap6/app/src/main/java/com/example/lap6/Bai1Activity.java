1eecc7eddf2b2464d0ac2bfe01fa9fc7f0fdd5ba
dinhthituyetchinh <fengdinh@gmail.com>
2024-04-08 22:49:06 +0700

    updates 10

--- HomeWork/Part1/Lap6/app/src/main/java/com/example/lap6/Bai1Activity.java
+++ HomeWork/Part1/Lap6/app/src/main/java/com/example/lap6/Bai1Activity.java
@@ -1,23 +1,25 @@
 package com.example.lap6;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Bai1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bai1);
 
-        Intent intent = new Intent(MainActivity.this, Bai1Activity.class);
-        startActivity(intent);
+        Toolbar toolbar = findViewById(R.id.toolbarBai1);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("FragmentDemo");
     }
 }
